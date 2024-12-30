f7de2f58347c302170c56ec60560361233b73a3a
tri <bactv816@gmail.com>
2024-12-30 17:26:45 +0700

    done

--- client/src/template/userTemplate/HeaderClient.vue
+++ client/src/template/userTemplate/HeaderClient.vue
@@ -2,7 +2,6 @@
     <div>
         <div class="text-[12px]">
             <span>Giảm giá 30% cho tất cả các sản phẩm đặc biệt trong tháng 11!</span>
-            <span>đây là header</span>
         </div>
         <div class="">
         </div>
