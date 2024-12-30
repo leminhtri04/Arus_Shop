aeb7007f9cb3b3c3a7a530344ed4eca2a3b401f6
tri <bactv816@gmail.com>
2024-12-30 17:08:46 +0700

    done setup vue

--- client/src/template/userTemplate/HeaderClient.vue
+++ client/src/template/userTemplate/HeaderClient.vue
@@ -4,14 +4,13 @@
             <span>Giảm giá 30% cho tất cả các sản phẩm đặc biệt trong tháng 11!</span>
         </div>
         <div class="">
-            <i class="pi pi-times" style="color: green"></i>
         </div>
         
     </div>
 </template>
 
 <script setup lang="ts">
-import 'primeicons/primeicons.css';
+
 </script>
 
 <style lang="scss" scoped>
--- client/src/template/userTemplate/UserTemplate.vue
+++ client/src/template/userTemplate/UserTemplate.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-    
+    import HeaderClient from './HeaderClient.vue';
 </script>
 
 <style lang="scss" scoped>
